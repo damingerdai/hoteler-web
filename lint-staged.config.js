cc6523f94286c7a48e7370866cf6e0eb9e3a280d
明国宾 <mingguobin@live.com>
2020-01-29 20:09:37 +0800

    ci: add commitlint and lint staged

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    "*.ts" : absolutePath => {
+        // const cwd = process.cwd();
+        // const relativePaths = absolutePath
+        return 'ng lint hoteler-web'
+    }
+}
