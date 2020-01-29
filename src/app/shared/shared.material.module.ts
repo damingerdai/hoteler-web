13439a9c6f30666525c3bd6cf585a9f22821295d
明国宾 <mingguobin@live.com>
2020-01-29 16:31:29 +0800

    feat(navbar): add link in nav bar

--- src/app/shared/shared.material.module.ts
+++ src/app/shared/shared.material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materials = [
+  MatButtonModule,
   MatToolbarModule
-]
+];
 
 
 @NgModule({
