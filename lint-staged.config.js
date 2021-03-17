d2df28501ecdca54afdc6502e0865ce666202bdf
Arthur Ming <guobin.ming@pwc.com>
2021-03-17 21:36:10 +0800

    ci: add prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    "**/*.ts?(x)": (filenames) =>
-		filenames.length > 10
-			? 'ng lint hoteler-web'
-			: `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
-					" "
-			  )}`,
-}
+  "**/*.ts?(x)": (filenames) =>
+    filenames.length > 10
+      ? "ng lint hoteler-web"
+      : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
+          " "
+        )}`,
+  "*.{html,md,json,yml,js,ts,css}": (filenames) => {
+    const prettier = `prettier --write -- ${filenames.join(" ")}`;
+    // const git = `git add ${filenames.join(" ")}`;
+    return [prettier];
+  },
+};
