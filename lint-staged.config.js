6ce8e0751a43660d5b35eccb6e9ead8c1dde815c
Arthur Ming <guobin.ming@pwc.com>
2021-06-04 11:45:45 +0800

    build: mirgrate to eslint from tslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,8 @@
-const path = require("path");
-
 module.exports = {
   "**/*.ts?(x)": (filenames) =>
     filenames.length > 10
       ? "ng lint hoteler-web"
-      : `tslint --format verbose --project ./tsconfig.json --config ./tslint.json ${filenames.join(
-          " "
-        )}`,
+      : `eslint --format stylish ${filenames.join(" ")}`,
   "*.{html,md,json,yml,js,css}": (filenames) => {
     const prettier = `prettier --write -- ${filenames.join(" ")}`;
     // const git = `git add ${filenames.join(" ")}`;
