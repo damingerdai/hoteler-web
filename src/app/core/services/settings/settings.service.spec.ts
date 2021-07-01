edd7e9a6ccf2e4e7a0d40fa7e8c406104d10dddc
Arthur Ming <guobin.ming@pwc.com>
2021-07-01 16:40:19 +0800

    test: SettingsService 'should be created' unit test pass

--- src/app/core/services/settings/settings.service.spec.ts
+++ src/app/core/services/settings/settings.service.spec.ts
@@ -1,13 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { LocalStorageService } from '../local-storage';
 
 import { SettingsService } from './settings.service';
 
 describe('SettingsService', () => {
   let service: SettingsService;
+  let localStorageService: LocalStorageService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        SettingsService,
+        {
+          provide: LocalStorageService,
+          useValue: {
+            set: (a: string, b: any) => void 0,
+            get: (a) => a,
+          }
+        }
+      ]
+    });
     service = TestBed.inject(SettingsService);
+    localStorageService = TestBed.inject(LocalStorageService);
   });
 
   it('should be created', () => {
