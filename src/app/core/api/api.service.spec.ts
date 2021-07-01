a301f1a3a5b29a4537f07acaba5ec954bdc69388
Arthur Ming <guobin.ming@pwc.com>
2021-07-01 20:46:31 +0800

    test: ApiService 'should be created' unit test pass

--- src/app/core/api/api.service.spec.ts
+++ src/app/core/api/api.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
@@ -6,7 +7,14 @@ describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        ApiService
+      ]
+    });
     service = TestBed.inject(ApiService);
   });
 
