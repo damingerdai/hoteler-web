57f149ceb4bc427f6c41691a2e563f8e267c6bd5
Arthur Ming <guobin.ming@pwc.com>
2021-07-01 21:02:54 +0800

    test: AuthInterceptor 'should be create' unit test pass

--- src/app/core/interceptors/auth.interceptor.spec.ts
+++ src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LocalStorageService } from '../services/local-storage';
+import { SettingsService } from '../services/settings/settings.service';
 
 import { AuthInterceptor } from './auth.interceptor';
 
 describe('AuthInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      MatDialogModule
+    ],
     providers: [
-      AuthInterceptor
-      ]
+      AuthInterceptor,
+      SettingsService,
+      LocalStorageService
+    ]
   }));
 
   it('should be created', () => {
