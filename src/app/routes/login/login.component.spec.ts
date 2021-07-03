ffdcc3c763244f7657e3524b6c61b9da57ae2fb1
Arthur Ming <guobin.ming@pwc.com>
2021-07-03 23:06:21 +0800

    test: login module 'should create' unit test pass

--- src/app/routes/login/login.component.spec.ts
+++ src/app/routes/login/login.component.spec.ts
@@ -1,4 +1,15 @@
+import { NgModule } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LocalStorageService } from 'src/app/core/services';
+import { SettingsService } from 'src/app/core/services/settings/settings.service';
+import { ApiTestingModule } from 'src/app/core/testings';
 
 import { LoginComponent } from './login.component';
 
@@ -8,6 +19,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        TestApp,
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -23,3 +37,21 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@NgModule({
+  providers: [
+    LocalStorageService,
+    SettingsService
+  ],
+  exports: [
+    BrowserModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    RouterTestingModule,
+    NoopAnimationsModule,
+    ApiTestingModule,
+  ]
+})
+class TestApp { }
