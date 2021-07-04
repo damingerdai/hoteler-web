e91559a66b4e09fea1c310b3430ef956c5f2923c
Arthur Ming <guobin.ming@pwc.com>
2021-07-04 12:24:45 +0800

    test: remove app comonent 'should render title' unit test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular app is running!');
-  });
 });
