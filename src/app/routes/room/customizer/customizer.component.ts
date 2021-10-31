f11bff2a21eee18e049db2d5977c035f18052a0c
Arthur Ming <guobin.ming@pwc.com>
2021-10-31 14:18:42 +0800

    feat: 房间列表支持卡片布局

---
+++ src/app/routes/room/customizer/customizer.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-customizer',
+  templateUrl: './customizer.component.html',
+  styleUrls: ['./customizer.component.scss']
+})
+export class CustomizerComponent implements OnInit {
+
+  public layouts = [
+    {
+      name: 'table',
+      display: '表格',
+      value: 'table'
+    },
+    {
+      name: 'card',
+      display: '卡片',
+      value: 'card'
+    }
+  ];
+
+  @Output()
+  public layoutChange = new EventEmitter<'table' | 'card'>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public changeLayout(layout: 'table' | 'card') {
+    this.layoutChange.emit(layout);
+  }
+
+}
