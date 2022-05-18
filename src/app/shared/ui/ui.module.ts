83444727f36bc704f7623174723f2d69c284efdf
Arthur Ming <guobin.ming@pwc.com>
2022-05-18 22:04:41 +0800

    feat: 添加matbutton的loading状态

--- src/app/shared/ui/ui.module.ts
+++ src/app/shared/ui/ui.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ButtonModule } from './button';
 import { DirectivesModule } from './directives/directives.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    ButtonModule,
     DirectivesModule
   ],
   exports: [
+    ButtonModule,
     DirectivesModule
   ]
 })
