6302bba2a87abc418509a53337e0fd67d19e1f3a
Arthur Ming <guobin.ming@pwc.com>
2022-05-20 15:52:00 +0800

    test: 测试button的loading状态时同时检测spinner组件是否存在

--- src/app/shared/ui/button/button-loading.directive.spec.ts
+++ src/app/shared/ui/button/button-loading.directive.spec.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { waitForAsync, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { By } from '@angular/platform-browser';
 import { ButtonLoadingDirective } from './button-loading.directive';
 import { ButtonModule } from './button.module';
@@ -26,6 +27,7 @@ describe('ButtonLoadingDirective', () => {
 
     TestBed.compileComponents();
   }));
+
   it('button loading', () => {
     const fixture = TestBed.createComponent(TestApp);
     const testComponent = fixture.debugElement.componentInstance;
@@ -34,9 +36,11 @@ describe('ButtonLoadingDirective', () => {
     testComponent.loading = true;
     fixture.detectChanges();
     expect(buttonDebugElement.nativeElement.classList.contains('mat-button-loading')).toBe(true);
-    expect(buttonNativeElement.disabled)
-      .withContext('Expected button to be disabled')
-      .toBeTrue();
+    expect(buttonNativeElement.disabled).withContext('Expected button to be disabled').toBeTrue();
+    const spinner1 = fixture.debugElement.query(
+      By.directive(MatProgressSpinner)
+    )!.componentInstance;
+    expect(spinner1).withContext('Expected spinner to be existed').toBeTruthy();
 
     testComponent.loading = false;
     fixture.detectChanges();
@@ -44,5 +48,9 @@ describe('ButtonLoadingDirective', () => {
     expect(buttonNativeElement.disabled)
       .withContext('Expected button not to be disabled')
       .toBeFalse();
+    const spinner2 = fixture.debugElement.query(
+      By.directive(MatProgressSpinner)
+    )?.componentInstance;
+    expect(spinner2).withContext('Expected spinner to be not existed').toBeFalsy();
   });
 });
