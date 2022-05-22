a0a7093737d030fa0fa3e37fbef5b9282f4df3b2
Arthur Ming <guobin.ming@pwc.com>
2022-05-22 19:30:55 +0800

    test: register view unit test fix

--- src/app/routes/register/register.component.spec.ts
+++ src/app/routes/register/register.component.spec.ts
@@ -1,4 +1,16 @@
+import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LocalStorageService } from 'src/app/core/services';
+import { SettingsService } from 'src/app/core/services/settings/settings.service';
+import { ApiTestingModule } from 'src/app/core/testings';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,6 +20,9 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        TestApp,
+      ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
@@ -23,3 +38,24 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+@NgModule({
+  providers: [
+    LocalStorageService,
+    SettingsService
+  ],
+  exports: [
+    BrowserModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+    RouterTestingModule,
+    NoopAnimationsModule,
+    ApiTestingModule,
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+class TestApp { }
