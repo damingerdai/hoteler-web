81104c71f342ccaf69b8cbf96f005575a0aae07f
Arthur Ming <guobin.ming@pwc.com>
2023-02-11 21:51:04 +0800

    style: remove 'N', 'M'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-import { M, N } from '@angular/cdk/keycodes';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
