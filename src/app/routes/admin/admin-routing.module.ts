5b57b74afa53fe0353ab198091b09caff63c77c2
damingerdai <mingguobin@live.com>
2023-03-02 22:16:04 +0800

    fix: 设置默认的admin页面

--- src/app/routes/admin/admin-routing.module.ts
+++ src/app/routes/admin/admin-routing.module.ts
@@ -9,24 +9,29 @@ const routes: Routes = [
     component: AdminComponent,
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+      },
+      {
         path: 'dashboard',
         title: 'Hoteler Portal -- 主页',
-        // canLoad: [AuthGuard],
-        // canActivate:  [AuthGuard],
+        canLoad: [AuthGuard],
+        canActivate:  [AuthGuard],
         loadChildren: () => import('../../routes/dashboard/dashboard.module').then(m => m.DashboardModule)
       },
       {
         path: 'room',
         title: 'Hoteler Portal -- 房间管理',
-        // canLoad: [AuthGuard],
-        // canActivate:  [AuthGuard],
+        canLoad: [AuthGuard],
+        canActivate:  [AuthGuard],
         loadChildren: () => import('../../routes/room/room.module').then(m => m.RoomModule)
       },
       {
         path: 'customer',
         title: 'Hoteler Portal -- 客户管理',
-        // canLoad: [AuthGuard],
-        // canActivate:  [AuthGuard],
+        canLoad: [AuthGuard],
+        canActivate:  [AuthGuard],
         loadChildren: () => import('../../routes/customer/customer.module').then(m => m.CustomerModule)
       },
     ]
