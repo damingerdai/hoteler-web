bd5749c19619d22f4363f0231fb77ce77e611e3d
Arthur Ming <guobin.ming@pwc.com>
2023-05-04 13:43:08 +0800

    style: remove `@typescript-eslint/naming-convention` and `@typescript-eslint/no-explicit-any` eslint rule

--- src/app/shared/components/skeleton-loader/skeleton-loader-config.types.ts
+++ src/app/shared/components/skeleton-loader/skeleton-loader-config.types.ts
@@ -3,7 +3,6 @@ import { InjectionToken } from '@angular/core';
 export type SkeletonLoaderConfigTheme = {
   // This is required since ngStyle is using `any` as well
   // More details in https://angular.io/api/common/NgStyle
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [k: string]: any;
 } | null;
 
