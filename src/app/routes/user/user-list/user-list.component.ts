8ab394bbce0462ba07fdbbf972e80d6eb33dbbec
Arthur Ming <guobin.ming@pwc.com>
2024-03-04 16:36:06 +0800

    feat: 当用户列表为空的时候显示没有数据

--- src/app/routes/user/user-list/user-list.component.ts
+++ src/app/routes/user/user-list/user-list.component.ts
@@ -3,27 +3,26 @@ import { IUser } from 'src/app/core/models';
 import { UserService } from 'src/app/core/services/user/user.service';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+    selector: 'app-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+    protected userService = inject(UserService);
 
-  protected userService = inject(UserService);
+    protected displayedColumns: string[] = ['id', 'username', 'roles'];
 
-  protected displayedColumns: string[] = ['id', 'username', 'roles'];
+    protected users: IUser[] = [];
 
-  protected users: IUser[] = [];
+    constructor() {
+        this.users = [];
+    }
 
-  constructor() {
-    this.users = [];
-  }
-
-  ngOnInit(): void {
-    this.userService.list().subscribe((res) => {
-      if (res.status === 200) {
-        this.users = res.data;
-      }
-    });
-  }
+    ngOnInit(): void {
+        this.userService.list().subscribe((res) => {
+            if (res.status === 200) {
+                this.users = res.data;
+            }
+        });
+    }
 }
