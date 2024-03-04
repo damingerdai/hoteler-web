43ae6a6149e238f5596264b871e8583e04423917
Arthur Ming <guobin.ming@pwc.com>
2024-03-04 20:59:05 +0800

    test: 修复UserlistComponent的独立组件模式单元测试的错误

--- src/app/routes/user/user-list/user-list.component.spec.ts
+++ src/app/routes/user/user-list/user-list.component.spec.ts
@@ -2,26 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
 import { ApiTestingModule } from 'src/app/core/testings';
+import { SharedMaterialModule } from 'src/app/shared/shared.material.module';
+import { SharedPipesModule } from 'src/app/shared/shared.pipes.module';
 
 describe('UserListComponent', () => {
-  let component: UserListComponent;
-  let fixture: ComponentFixture<UserListComponent>;
+    let component: UserListComponent;
+    let fixture: ComponentFixture<UserListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        ApiTestingModule,
-      ],
-      declarations: [ UserListComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                ApiTestingModule,
+                SharedMaterialModule,
+                SharedPipesModule,
 
-    fixture = TestBed.createComponent(UserListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+                UserListComponent,
+            ],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(UserListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
