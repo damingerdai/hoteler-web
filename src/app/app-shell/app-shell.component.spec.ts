d323f57342d18c88b4fefad5d20f86e072e1ea2b
Arthur Ming <guobin.ming@pwc.com>
2024-08-18 21:03:39 +0800

    test: 修复app shell的测试用例报错

--- src/app/app-shell/app-shell.component.spec.ts
+++ src/app/app-shell/app-shell.component.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppShellComponent } from './app-shell.component';
+import { SharedMaterialModule } from '../shared/shared.material.module';
+import { CoreModule } from '../core';
+import { provideRouter } from '@angular/router';
+import { SettingsService } from '../core/services/settings/settings.service';
+import { ThemeStorageService } from '../shared/components';
 
 describe('AppShellComponent', () => {
-  let component: AppShellComponent;
-  let fixture: ComponentFixture<AppShellComponent>;
+    let component: AppShellComponent;
+    let fixture: ComponentFixture<AppShellComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppShellComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CoreModule, SharedMaterialModule, AppShellComponent],
+            providers: [
+                provideRouter([]),
+                SettingsService,
+                ThemeStorageService,
+            ],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(AppShellComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(AppShellComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
