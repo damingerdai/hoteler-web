22629d0d75f54cf24a33bcfa21de0909f7e31773
Arthur Ming <guobin.ming@pwc.com>
2024-08-30 21:22:15 +0800

    refactor: 使用ngx-apexcharts中基于指令渲染图表

--- src/app/routes/dashboard/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard/dashboard.component.ts
@@ -13,108 +13,116 @@ import { MatCardModule } from '@angular/material/card';
 type ApexOptions2 = ApexOptions & { show: boolean };
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
-  standalone: true,
-  imports: [
-    NgxApexchartsModule,
-
-    MatCardModule,
-
-    LoadingShadeComponent,
-    BreadcrumbComponent,
-  ]
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
+    standalone: true,
+    imports: [
+        NgxApexchartsModule,
+        MatCardModule,
+        LoadingShadeComponent,
+        BreadcrumbComponent,
+    ],
 })
 export class DashboardComponent implements OnInit {
+    protected loading = false;
 
-  protected loading = false;
+    private customerApi: CustomerService = inject(CustomerService);
+    private customerCheckinRecordApi: CustomerCheckinRecordService = inject(
+        CustomerCheckinRecordService
+    );
+    private roomApi: RoomService = inject(RoomService);
+    private dialog = inject(MatDialog);
 
-  private customerApi: CustomerService = inject(CustomerService);
-  private customerCheckinRecordApi: CustomerCheckinRecordService = inject(CustomerCheckinRecordService);
-  private roomApi: RoomService = inject(RoomService);
-  private dialog = inject(MatDialog);
-
-  public roomStatusDonutChart: ApexOptions2 = {
-    show: false,
-    chart: {
-      type: 'donut',
-    },
-    title: {
-      text: '房间状态统计'
-    },
-    series: [44, 55, 13, 33],
-    labels: ['Apple', 'Mango', 'Orange', 'Watermelon'],
-    plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true,
-            name: {
-              show: true,
+    public roomStatusDonutChart: ApexOptions2 = {
+        show: false,
+        chart: {
+            type: 'donut',
+        },
+        title: {
+            text: '房间状态统计',
+        },
+        series: [44, 55, 13, 33],
+        labels: ['Apple', 'Mango', 'Orange', 'Watermelon'],
+        plotOptions: {
+            pie: {
+                donut: {
+                    labels: {
+                        show: true,
+                        name: {
+                            show: true,
+                        },
+                        value: {
+                            show: true,
+                        },
+                        total: {
+                            show: true,
+                        },
+                    },
+                },
             },
-            value: {
-              show: true,
-            },
-            total: {
-              show: true,
+        },
+    };
+
+    public pastWeekCustomerCountChart: ApexOptions2 = {
+        show: false,
+        chart: {
+            type: 'line',
+        },
+        title: {
+            text: '过去一周的入住客户数量变化',
+        },
+    };
+
+    constructor() {}
+
+    openAddUserRoomDialog() {
+        const dialogRef = this.dialog.open(AddUserRoomComponent);
+        dialogRef
+            .afterClosed()
+            .pipe(
+                filter((res) => !!res),
+                switchMap((ur) => this.customerCheckinRecordApi.create(ur))
+            )
+            .subscribe((res) => {
+                if (res.status === 200) {
+                    console.log('test');
+                }
+            });
+    }
+
+    ngOnInit(): void {
+        this.loading = true;
+        this.roomApi.getRoomStatusStat().subscribe((res) => {
+            this.loading = false;
+            if (res.status === 200) {
+                this.roomStatusDonutChart.show = true;
+                this.roomStatusDonutChart.series = [
+                    res.data.inUseNums,
+                    res.data.notUsedNums,
+                ];
+                this.roomStatusDonutChart.labels = ['占用', '空闲'];
             }
-          }
-        }
-      }
+        });
+
+        this.customerApi.getPastWeekCustomerCountStat().subscribe((res) => {
+            this.loading = false;
+            if (res.status === 200) {
+                this.pastWeekCustomerCountChart.show = true;
+                this.pastWeekCustomerCountChart.series = [
+                    {
+                        name: '入住客户数量',
+                        data: res.data.pastWeekCustomerCounts.map(
+                            (v) => v.customerCount
+                        ),
+                    },
+                ];
+                this.pastWeekCustomerCountChart.xaxis = {
+                    categories: res.data.pastWeekCustomerCounts.map(
+                        (v) => v.checkInDate
+                    ),
+                };
+            }
+        });
     }
-  };
-
-  public pastWeekCustomerCountChart: ApexOptions2 = {
-    show: false,
-    chart: {
-      type: 'line',
-    },
-    title: {
-      text: '过去一周的入住客户数量变化'
-    }
-  };
-
-  constructor() {}
-
-  openAddUserRoomDialog() {
-    const dialogRef = this.dialog.open(AddUserRoomComponent);
-    dialogRef.afterClosed()
-      .pipe(
-        filter(res => !!res),
-        switchMap(ur => this.customerCheckinRecordApi.create(ur))
-      ).subscribe(res => {
-        if (res.status === 200) {
-          console.log('test');
-        }
-      });
-  }
-
-  ngOnInit(): void {
-    this.loading = true;
-    this.roomApi.getRoomStatusStat().subscribe(res => {
-      this.loading = false;
-      if (res.status === 200) {
-        this.roomStatusDonutChart.show = true;
-        this.roomStatusDonutChart.series = [res.data.inUseNums, res.data.notUsedNums];
-        this.roomStatusDonutChart.labels = ['占用', '空闲'];
-      }
-    });
-
-
-    this.customerApi.getPastWeekCustomerCountStat().subscribe(res => {
-      this.loading = false;
-      if (res.status === 200) {
-        this.pastWeekCustomerCountChart.show = true;
-        this.pastWeekCustomerCountChart.series = [{
-          name: '入住客户数量',
-          data: res.data.pastWeekCustomerCounts.map(v => v.customerCount),
-        }];
-        this.pastWeekCustomerCountChart.xaxis = {
-          categories: res.data.pastWeekCustomerCounts.map(v => v.checkInDate)
-        };
-      }
-    });
-  }
-
 }
