079017b59d9af5d6b780393be22f11711f8ed957
Arthur Ming <guobin.ming@pwc.com>
2024-09-04 16:17:16 +0800

    test: 修复UpdateUserComponent的单元测试

--- src/app/routes/user/dialog/update-user/update-user.component.spec.ts
+++ src/app/routes/user/dialog/update-user/update-user.component.spec.ts
@@ -1,6 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpdateUserComponent } from './update-user.component';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { NgModule } from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  exports: [
+    BrowserModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    A11yModule
+  ]
+})
+class TestApp { }
 
 describe('UpdateUserComponent', () => {
   let component: UpdateUserComponent;
@@ -8,9 +32,34 @@ describe('UpdateUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UpdateUserComponent]
+      imports: [TestApp, UpdateUserComponent],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            roles: [
+              {
+                id: '1',
+                name: 'admin',
+                description: 'admin role',
+              }
+            ],
+            user: {
+              username: 'test',
+              password: 'test',
+              roles: [
+                {
+                  id: '1',
+                  name: 'admin',
+                  description: 'admin role',
+                }
+              ]
+            }
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(UpdateUserComponent);
     component = fixture.componentInstance;
