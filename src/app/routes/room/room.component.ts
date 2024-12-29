2ad38bfe6477bc690801d5b5d8b1400aa590baa6
damingerdai <mingguobin@live.com>
2024-12-29 21:09:12 +0800

    feat: 使用tab切换room的卡片和表格模式

--- src/app/routes/room/room.component.ts
+++ src/app/routes/room/room.component.ts
@@ -1,4 +1,3 @@
-
 import { filter, map, switchMap, tap } from 'rxjs/operators';
 
 import { Component, OnInit } from '@angular/core';
@@ -9,21 +8,30 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+
 import { CustomizerComponent } from './customizer/customizer.component';
 import { RoomService } from 'src/app/core/services/room';
 import { IRoom, Rooms } from 'src/app/core/models';
 import { of } from 'rxjs';
 import { CustomerCheckinRecordService } from 'src/app/core/services/customer-checkin-record';
 import { CurrencyPipe } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSelectModule } from '@angular/material/select';
-import { AddCustomerRoomDialogComponent, CreateRoomDialogComponent, UpdateRoomDialogComponent } from './dialog';
-import { BreadcrumbComponent, ConfirmComponent, TagComponent } from 'src/app/shared/components';
+import {
+    AddCustomerRoomDialogComponent,
+    CreateRoomDialogComponent,
+    UpdateRoomDialogComponent,
+} from './dialog';
+import {
+    BreadcrumbComponent,
+    ConfirmComponent,
+    TagComponent,
+} from 'src/app/shared/components';
 import { LoadingShadeComponent } from 'src/app/shared/components/loading-shade';
-
 
 @Component({
     selector: 'app-menu',
@@ -33,6 +41,7 @@ import { LoadingShadeComponent } from 'src/app/shared/components/loading-shade';
         MatMenuModule,
         MatButtonModule,
         MatIconModule,
+        MatTabsModule,
         MatTableModule,
         MatCardModule,
         MatProgressSpinnerModule,
@@ -45,222 +54,231 @@ import { LoadingShadeComponent } from 'src/app/shared/components/loading-shade';
         BreadcrumbComponent,
         CustomizerComponent,
         TagComponent,
-    ]
+    ],
 })
 export class RoomComponent implements OnInit {
+    public layout = 'card';
 
-  public layout = 'card';
+    public displayedColumns: string[] = [
+        'no',
+        'roomname',
+        'price',
+        'status',
+        'action',
+    ];
 
-  public displayedColumns: string[] = [
-    'no',
-    'roomname',
-    'price',
-    'status',
-    'action',
-  ];
+    public rooms: Rooms;
 
-  public rooms: Rooms;
+    public roomForm: FormGroup;
 
-  public roomForm: FormGroup;
+    public isLoading: boolean;
 
-  public isLoading: boolean;
+    constructor(
+        private dialog: MatDialog,
+        private fb: FormBuilder,
+        private roomApi: RoomService,
+        private customerCheckinRecordApi: CustomerCheckinRecordService,
+        private snackBar: MatSnackBar
+    ) {
+        this.isLoading = true;
+        this.roomForm = this.fb.group({
+            status: [0],
+        });
 
-  constructor(
-    private dialog: MatDialog,
-    private fb: FormBuilder,
-    private roomApi: RoomService,
-    private customerCheckinRecordApi: CustomerCheckinRecordService,
-    private snackBar: MatSnackBar,
-  ) {
-    this.isLoading = true;
-    this.roomForm = this.fb.group({
-      status: [0],
-    });
-
-    this.roomForm
-      .get('status')
-      .valueChanges.pipe(
-        tap(() => (this.isLoading = true)),
-        map((res) => (typeof res !== 'number' ? parseInt(res, 10) : res)),
-        switchMap((s) =>
-          s ? this.roomApi.list({ status: s }) : this.roomApi.list(),
-        ),
-      )
-      .subscribe({
-        next: (res) => {
-          if (res.status === 200) {
-            this.rooms = res.data;
-          }
-          this.isLoading = false;
-        },
-        error: () => (this.isLoading = false),
-      });
-  }
-
-  ngOnInit(): void {
-    this.fetchAllRooms();
-  }
-
-  public createRoom() {
-    const dialogRef = this.dialog.open(CreateRoomDialogComponent, {
-      width: '400px',
-    });
-
-    dialogRef
-      .afterClosed()
-      .pipe(
-        filter((res) => !!res),
-        switchMap((res) => this.roomApi.create(res)),
-      )
-      .subscribe(
-        () => {
-          this.snackBar.open('创建房间成功🚀', 'X', {
-            duration: 500,
-            horizontalPosition: 'right',
-            verticalPosition: 'top',
-          });
-        },
-        (err) => {
-          console.error(err);
-        },
-        () => this.fetchAllRooms(),
-      );
-  }
-
-  public updateRoom(room: IRoom) {
-    const dialogRef = this.dialog.open(UpdateRoomDialogComponent, {
-      width: '400px',
-      data: room,
-    });
-    dialogRef
-      .afterClosed()
-      .pipe(
-        filter((res) => !!res),
-        map((res) => {
-          return {
-            ...room,
-            ...res,
-          };
-        }),
-        switchMap((res) => this.roomApi.update(res)),
-      )
-      .subscribe(
-        (res) => {
-          if (res.status === 200) {
-            this.snackBar.open('更新房间成功🚀', 'X', {
-              duration: 500,
-              horizontalPosition: 'right',
-              verticalPosition: 'top',
+        this.roomForm
+            .get('status')
+            .valueChanges.pipe(
+                tap(() => (this.isLoading = true)),
+                map((res) =>
+                    typeof res !== 'number' ? parseInt(res, 10) : res
+                ),
+                switchMap((s) =>
+                    s ? this.roomApi.list({ status: s }) : this.roomApi.list()
+                )
+            )
+            .subscribe({
+                next: (res) => {
+                    if (res.status === 200) {
+                        this.rooms = res.data;
+                    }
+                    this.isLoading = false;
+                },
+                error: () => (this.isLoading = false),
             });
-          } else {
-            const message = res.error.message;
-            this.snackBar.open('更新房间失败🚀：' + message, 'X', {
-              duration: 500,
-              horizontalPosition: 'right',
-              verticalPosition: 'top',
+    }
+
+    ngOnInit(): void {
+        this.fetchAllRooms();
+    }
+
+    public createRoom() {
+        const dialogRef = this.dialog.open(CreateRoomDialogComponent, {
+            width: '400px',
+        });
+
+        dialogRef
+            .afterClosed()
+            .pipe(
+                filter((res) => !!res),
+                switchMap((res) => this.roomApi.create(res))
+            )
+            .subscribe(
+                () => {
+                    this.snackBar.open('创建房间成功🚀', 'X', {
+                        duration: 500,
+                        horizontalPosition: 'right',
+                        verticalPosition: 'top',
+                    });
+                },
+                (err) => {
+                    console.error(err);
+                },
+                () => this.fetchAllRooms()
+            );
+    }
+
+    public updateRoom(room: IRoom) {
+        const dialogRef = this.dialog.open(UpdateRoomDialogComponent, {
+            width: '400px',
+            data: room,
+        });
+        dialogRef
+            .afterClosed()
+            .pipe(
+                filter((res) => !!res),
+                map((res) => {
+                    return {
+                        ...room,
+                        ...res,
+                    };
+                }),
+                switchMap((res) => this.roomApi.update(res))
+            )
+            .subscribe(
+                (res) => {
+                    if (res.status === 200) {
+                        this.snackBar.open('更新房间成功🚀', 'X', {
+                            duration: 500,
+                            horizontalPosition: 'right',
+                            verticalPosition: 'top',
+                        });
+                    } else {
+                        const message = res.error.message;
+                        this.snackBar.open('更新房间失败🚀：' + message, 'X', {
+                            duration: 500,
+                            horizontalPosition: 'right',
+                            verticalPosition: 'top',
+                        });
+                    }
+                },
+                (err) => {
+                    console.error(err);
+                },
+                () => this.fetchAllRooms()
+            );
+    }
+
+    public deleteRoom(id: string | number) {
+        const dialogRef = this.dialog.open(ConfirmComponent, {
+            width: '400px',
+            data: {
+                title: '确定删除',
+                description: '请确定是否删除？',
+            },
+        });
+        dialogRef
+            .afterClosed()
+            .pipe(
+                filter((res) => res === true),
+                switchMap(() =>
+                    this.roomApi.delete(
+                        typeof id === 'string' ? parseInt(id, 10) : id
+                    )
+                )
+            )
+            .subscribe((res) => {
+                if (res.status === 200) {
+                    this.snackBar.open('删除房间成功🚀', 'X', {
+                        duration: 500,
+                        horizontalPosition: 'right',
+                        verticalPosition: 'top',
+                    });
+                    this.fetchAllRooms();
+                } else {
+                    this.snackBar.open(
+                        '删除房间失败:' + res.error.message,
+                        'X',
+                        {
+                            duration: 500,
+                            horizontalPosition: 'right',
+                            verticalPosition: 'top',
+                        }
+                    );
+                }
             });
-          }
-        },
-        (err) => {
-          console.error(err);
-        },
-        () => this.fetchAllRooms(),
-      );
-  }
+    }
 
-  public deleteRoom(id: string | number) {
-    const dialogRef = this.dialog.open(ConfirmComponent, {
-      width: '400px',
-      data: {
-        title: '确定删除',
-        description: '请确定是否删除？',
-      },
-    });
-    dialogRef
-      .afterClosed()
-      .pipe(
-        filter((res) => res === true),
-        switchMap(() =>
-          this.roomApi.delete(typeof id === 'string' ? parseInt(id, 10) : id),
-        ),
-      )
-      .subscribe((res) => {
-        if (res.status === 200) {
-          this.snackBar.open('删除房间成功🚀', 'X', {
-            duration: 500,
-            horizontalPosition: 'right',
-            verticalPosition: 'top',
-          });
-          this.fetchAllRooms();
-        } else {
-          this.snackBar.open('删除房间失败:' + res.error.message, 'X', {
-            duration: 500,
-            horizontalPosition: 'right',
-            verticalPosition: 'top',
-          });
-        }
-      });
-  }
+    public addCustomerRoomDialog(room: IRoom) {
+        const dialogRef = this.dialog.open(AddCustomerRoomDialogComponent, {
+            width: '400px',
+            data: room,
+        });
 
-  public addCustomerRoomDialog(room: IRoom) {
-    const dialogRef = this.dialog.open(AddCustomerRoomDialogComponent, {
-      width: '400px',
-      data: room,
-    });
+        dialogRef
+            .afterClosed()
+            .pipe(
+                filter((res) => !!res),
+                map((res) => {
+                    const checkInTime = res.checkInTime as {
+                        beginDate: Date;
+                        endDate: Date;
+                    };
+                    const beginDate = checkInTime.beginDate;
+                    beginDate.setHours(12, 0, 0);
+                    const endDate = checkInTime.endDate;
+                    endDate.setHours(12, 0, 0);
+                    res.beginDate = beginDate;
+                    res.endDate = endDate;
+                    return res;
+                }),
+                switchMap((res) => this.customerCheckinRecordApi.create(res))
+            )
+            .subscribe((res) => {
+                if (res.status === 200) {
+                    this.snackBar.open('入住成功🚀', 'X', {
+                        duration: 500,
+                        horizontalPosition: 'right',
+                        verticalPosition: 'top',
+                    });
+                    this.fetchAllRooms();
+                }
+            });
+    }
 
-    dialogRef
-      .afterClosed()
-      .pipe(
-        filter((res) => !!res),
-        map((res) => {
-          const checkInTime = res.checkInTime as {
-            beginDate: Date;
-            endDate: Date;
-          };
-          const beginDate = checkInTime.beginDate;
-          beginDate.setHours(12, 0, 0);
-          const endDate = checkInTime.endDate;
-          endDate.setHours(12, 0, 0);
-          res.beginDate = beginDate;
-          res.endDate = endDate;
-          return res;
-        }),
-        switchMap((res) => this.customerCheckinRecordApi.create(res)),
-      )
-      .subscribe((res) => {
-        if (res.status === 200) {
-          this.snackBar.open('入住成功🚀', 'X', {
-            duration: 500,
-            horizontalPosition: 'right',
-            verticalPosition: 'top',
-          });
-          this.fetchAllRooms();
-        }
-      });
-  }
+    public layoutChange(layout: string) {
+        this.layout = layout;
+    }
 
-  public layoutChange(layout: string) {
-    this.layout = layout;
-  }
-
-  private fetchAllRooms() {
-    this.isLoading = true;
-    const status = this.roomForm.get('status').value;
-    of(status)
-      .pipe(
-        map((res) => (typeof res !== 'number' ? parseInt(res, 10) : res)),
-        switchMap((s) =>
-          s ? this.roomApi.list({ status: s }) : this.roomApi.list(),
-        ),
-      )
-      .subscribe({
-        next: (res) => {
-          if (res.status === 200) {
-            this.rooms = res.data;
-          }
-          this.isLoading = false;
-        },
-        error: () => (this.isLoading = false),
-      });
-  }
+    private fetchAllRooms() {
+        this.isLoading = true;
+        const status = this.roomForm.get('status').value;
+        of(status)
+            .pipe(
+                map((res) =>
+                    typeof res !== 'number' ? parseInt(res, 10) : res
+                ),
+                switchMap((s) =>
+                    s ? this.roomApi.list({ status: s }) : this.roomApi.list()
+                )
+            )
+            .subscribe({
+                next: (res) => {
+                    if (res.status === 200) {
+                        this.rooms = res.data;
+                    }
+                    this.isLoading = false;
+                },
+                error: () => (this.isLoading = false),
+            });
+    }
 }
