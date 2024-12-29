c01c68bdc5d93dcc4053b3dff48c37ad64a9850e
damingerdai <mingguobin@live.com>
2024-12-29 14:43:31 +0800

    feat: close angular-eslint standalone rule for spectifc components

--- eslint.config.js
+++ eslint.config.js
@@ -90,6 +90,7 @@ module.exports = tseslint.config(
                     style: 'kebab-case',
                 },
             ],
+            '@angular-eslint/prefer-standalone': ['warn'],
         },
     },
     {
