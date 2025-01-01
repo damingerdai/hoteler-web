ab676ebd8ddc02e5c85dfc68162adc362f979cd7
damingerdai <mingguobin@live.com>
2025-01-01 21:26:58 +0800

    fix: 添加provideNativeDateAdapter

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
-  ApplicationConfig,
-  isDevMode,
-} from '@angular/core';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+    provideHttpClient,
+    withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
+    provideRouter,
+    withComponentInputBinding,
+    withInMemoryScrolling,
 } from '@angular/router';
 import { routes } from './app.route';
 import { httpInterceptorProviders } from './core/interceptors';
@@ -15,29 +15,31 @@ import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 import { appInitializerProviders } from './core/initializers';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideAngularToaster } from 'angular-toaster';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideAnimationsAsync(),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      }),
-      withComponentInputBinding()
-    ),
-    httpInterceptorProviders,
-    {
-      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
-      useValue: { duration: 2500 },
-    },
-    appInitializerProviders,
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideAngularToaster(),
-  ],
+    providers: [
+        provideAnimationsAsync(),
+        provideRouter(
+            routes,
+            withInMemoryScrolling({
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+            }),
+            withComponentInputBinding()
+        ),
+        httpInterceptorProviders,
+        {
+            provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+            useValue: { duration: 2500 },
+        },
+        appInitializerProviders,
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideNativeDateAdapter(),
+        provideAngularToaster(),
+    ],
 };
