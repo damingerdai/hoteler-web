08ccad6413c7c34cd51b9d2dccf04787f4ea9cec
damingerdai <mingguobin@live.com>
2025-01-19 11:29:01 +0800

    test: fix order component unit test case

--- src/app/routes/orders/orders.component.spec.ts
+++ src/app/routes/orders/orders.component.spec.ts
@@ -2,26 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrdersComponent } from './orders.component';
 import { provideRouter } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('OrdersComponent', () => {
-  let component: OrdersComponent;
-  let fixture: ComponentFixture<OrdersComponent>;
+    let component: OrdersComponent;
+    let fixture: ComponentFixture<OrdersComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [OrdersComponent],
-      providers: [
-        provideRouter([])
-      ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, OrdersComponent],
+            providers: [provideRouter([])],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(OrdersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(OrdersComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
