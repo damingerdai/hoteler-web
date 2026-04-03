2e4942f2bccee9cc5d55550192896aac9ca0fb2d
damingerdai <mingguobin@live.com>
2026-04-03 20:14:33 +0800

    feat: toggle theme button support m3

--- src/app/shared/components/toggle-button/toggle-button.component.ts
+++ src/app/shared/components/toggle-button/toggle-button.component.ts
@@ -1,25 +1,57 @@
-import { Component, DOCUMENT, inject, Renderer2, signal } from '@angular/core';
+import { Component, DOCUMENT, inject, OnInit, Renderer2, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-toggle-button',
   imports: [
     MatButtonModule,
+    MatTooltipModule,
     MatIconModule,
   ],
   templateUrl: './toggle-button.component.html',
   styleUrl: './toggle-button.component.scss',
 })
-export class ToggleButtonComponent {
+export class ToggleButtonComponent implements OnInit {
+
 
   private renderer = inject(Renderer2);
   private document = inject(DOCUMENT);
 
-  protected isDarkMode = signal(false);
+  protected isDarkMode = signal(false)
 
   protected toggleTheme() {
-    this.isDarkMode.set(!this.isDarkMode());
-    this.renderer.setStyle(this.document.documentElement, 'color-scheme', this.isDarkMode() ? 'light' : 'dark');
+    const newMode = !this.isDarkMode();
+    this.isDarkMode.set(newMode);
+    this.updateThemeDOM(newMode);
+    localStorage.setItem('theme', newMode ? 'dark' : 'light');
+  }
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    const isDark = savedTheme === 'dark' || (!savedTheme && prefersDark);
+
+    this.isDarkMode.set(isDark);
+    this.updateThemeDOM(isDark);
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+      if (!localStorage.getItem('theme')) { // 仅当用户没手动选死主题时跟随系统
+        this.isDarkMode.set(e.matches);
+        this.updateThemeDOM(e.matches);
+      }
+    });
+  }
+
+  private updateThemeDOM(isDark: boolean) {
+    if (isDark) {
+      this.renderer.addClass(this.document.documentElement, 'dark-theme');
+      this.renderer.setStyle(this.document.documentElement, 'color-scheme', 'dark');
+    } else {
+      this.renderer.removeClass(this.document.documentElement, 'dark-theme');
+      this.renderer.setStyle(this.document.documentElement, 'color-scheme', 'light');
+    }
   }
 }
